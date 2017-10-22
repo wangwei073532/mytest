01a62cbc4593496afa4eeafc913d746494d80383
wangwei073532 <wangwei073532@users.noreply.github.com>
2017-10-22 20:35:46 +0800

    输出

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './route.config';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import JSApi from './modules/common/services/jsapi';
-import ShopService from './modules/shop/services';
-JSApi.triggerLogin();
+
 if (!sessionStorage.getItem('userInfo')) {
   ShopService.fetchUserInfo().then((userInfo) => {
     if(parseInt(userInfo.retCode, 10) === 0) {
