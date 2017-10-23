fbee6357ca6ef23554c46955dbc83ea90e4f09fd
wangwei073532 <wangwei073532@users.noreply.github.com>
2017-10-23 23:50:02 +0800

    apple

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-
-if (!sessionStorage.getItem('userInfo')) {
-  ShopService.fetchUserInfo().then((userInfo) => {
-    if(parseInt(userInfo.retCode, 10) === 0) {
-      sessionStorage.setItem('userInfo', JSON.stringify(userInfo));
-    } else {
-      sessionStorage.removeItem('userInfo');
-    }sssss
-  });
-}
-ReactDOM.render(
-  <Root />,
-  document.getElementById('root')
-);
-window.setCMToken = JSApi.generateToken;
+const apple ='APPLE';
+const orange ='ORANGE';
